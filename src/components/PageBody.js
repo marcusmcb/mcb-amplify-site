f6959b495f6ba0be4cfd9142463efc37b63db970
Marcus McBride <marcusmcb@gmail.com>
2021-05-28 21:25:34 -0700

    added bg color to pagebody contents

--- src/components/PageBody.js
+++ src/components/PageBody.js
@@ -4,7 +4,7 @@ import './pagebody.css'
 
 const PageBody = () => {
   return (
-    <div>
+    <div className='page-body'>
       <Row className='g-0 page-body-contents'>
         <Col sm={4}>
           <Card className='card-border content-body'>
@@ -15,25 +15,29 @@ const PageBody = () => {
                 src='https://avatars.githubusercontent.com/u/38328362?v=4'
                 alt='MCB Logo'
                 width='200'
-                fluid roundedCircle               
+                fluid
+                roundedCircle
               />
             </Card.Body>
           </Card>
         </Col>
-        <Col sm={8}>
+        <Col md={8}>
           <Card className='card-border content-body'>
             <Card.Body>
               <Card.Text>
-                <p>
-                  Marcus here. I'm a full-stack developer and broadcast producer
-                  based in Orange County, California
-                </p>
-                <p>
-                  I've worked as a contract producer, programmer and project
-                  manager within the commercial radio industry for the past two
-                  decades.
-                </p>
-                <p>I also write code!</p>
+                Marcus here. I'm a full-stack developer and broadcast producer
+                based in Orange County, California
+                <br />
+                <br />
+                I've worked as a contract producer, programmer and project
+                manager within the commercial radio industry for the past two
+                decades. 
+                <br />
+                <br />
+                I also write code!
+                <br />
+                <br />
+                There will be a lot more text here about this, that and the other thing which I'm sure will be super interesting to read but right now I'm just entering text to see how it formats on mobile view.
               </Card.Text>
             </Card.Body>
           </Card>
