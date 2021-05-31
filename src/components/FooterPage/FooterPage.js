9382719720e739720cd561494ad9beadb67d71e7
Marcus McBride <marcusmcb@gmail.com>
2021-05-30 19:32:49 -0700

    added styling to footerpage

--- src/components/FooterPage/FooterPage.js
+++ src/components/FooterPage/FooterPage.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React from 'react'
+import { Row, Card } from 'react-bootstrap'
 
 import Navbar from '../shared/Navbar'
 import Footer from '../shared/Footer'
 
-import './footerpage.css';
+import './footerpage.css'
 
 const FooterPage = () => {
-    return (
-        <div className='footer-page'>            
-            <Navbar/>
-            <h1 className='footer-page-text'>HERE</h1>
-            <Footer/>
-        </div>
-    )
+  return (
+    <div className='footer-page'>
+      <Navbar />
+      <Row className='footer-page-body g-0'>
+        <Card.Body>
+          <h1 className='footer-page-title'>About This Site</h1>
+          <div  className='footer-page-text'>
+            <p>
+              Built with React and Amplify (and a boat-load of
+              custom CSS).
+            </p>
+            <p>
+                Mobile-first responsive design tested in iOS, Chrome and Safari.
+            </p>
+          </div>
+        </Card.Body>
+      </Row>
+      <Footer />
+    </div>
+  )
 }
 
-export default FooterPage;
+export default FooterPage
