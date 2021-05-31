1360d88fa5b3181e4646b120ac9911c8fa7745fc
Marcus McBride <marcusmcb@gmail.com>
2021-05-30 22:03:30 -0700

    added boilerplate for nav pages

---
+++ src/components/ContactPage/ContactPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../shared/Navbar'
+import Footer from '../shared/Footer'
+
+import './contactpage.css'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <Navbar/>
+            <h1 className='contact-page-title'>CONTACT PAGE</h1>
+            <Footer/>
+        </div>
+    )
+}
+
+export default ContactPage;
