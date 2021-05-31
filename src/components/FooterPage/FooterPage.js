4557195544c9edf01399999fd00157a1c54a9faa
Marcus McBride <marcusmcb@gmail.com>
2021-05-31 15:40:03 -0700

    added git link to footerpage blurb

--- src/components/FooterPage/FooterPage.js
+++ src/components/FooterPage/FooterPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Row, Card } from 'react-bootstrap'
+import { Row, Card, Button } from 'react-bootstrap'
 
 import Navbar from '../shared/Navbar'
 import Footer from '../shared/Footer'
@@ -13,14 +13,24 @@ const FooterPage = () => {
       <Row className='footer-page-body g-0'>
         <Card.Body>
           <h1 className='footer-page-title'>About This Site</h1>
-          <div  className='footer-page-text'>
+          <div className='footer-page-text'>
+            <p>Built with React and Amplify (and a boat-load of custom CSS).</p>
             <p>
-              Built with React and Amplify (and a boat-load of
-              custom CSS).
+              Mobile-first responsive design tested in iOS, Chrome and Safari.
             </p>
-            <p>
-                Mobile-first responsive design tested in iOS, Chrome and Safari.
-            </p>
+            <span className='footer-page-button-row'>
+              <Button className='devpage-button' variant='secondary'>
+                <a
+                  href='https://github.com/marcusmcb/mcb-amplify-site'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  View Code
+                </a>
+              </Button>
+            </span>
+            <h3 className='footer-page-title'>Errors or site issues?</h3>
+            <p>Send them to me at marcusmcb@gmail.com.</p>
           </div>
         </Card.Body>
       </Row>
