32c0d428a6d94eedb994f5a464cb831dfb1f9bc8
Marcus McBride <marcusmcb@gmail.com>
2021-06-01 11:48:40 -0700

    added styling to contact page box

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -11,28 +11,50 @@ const ContactPage = () => {
     <div>
       <Navbar />
       <div className='contact-page-body'>
-        <Row>
+        <Row className='g-0'>
           <Col sm={6}>ONE</Col>
-          <Col sm={6}>
-            <Card>
-              <Card.Body>
-                <Card.Title className='contact-page-title'>
-                  <h3 className='contact-title'>Contact:</h3>
-                </Card.Title>
-                <span className='contact-icon-row'>
-                  <i className='fa fa-phone contact-icon'></i>
-                  <h4>714-889-8793</h4>
-                </span>
-                <span className='contact-icon-row'>                  
-                <i class="fa fa-envelope contact-icon"></i>
-                  <h4>marcusmcb@gmail.com</h4>
-                </span>
-                <span className='contact-icon-row'>                  
-                <i class="fa fa-link contact-icon"></i>
-                  <h4>linktree</h4>
-                </span>
-              </Card.Body>
-            </Card>
+          <Col sm={6} className='second-column'>
+            <div>
+              <Card className='contact-page-card-body'>
+                <Card.Body className='contact-page-transparency'>
+                  <Card.Title className='contact-page-title'>
+                    <h3 className='contact-title'>Contact:</h3>
+                  </Card.Title>
+                  <span className='contact-icon-row'>
+                    <i className='fa fa-phone contact-icon'></i>
+                    <h4 className='contact-icon-title'>714-889-8793</h4>
+                  </span>
+                  <span className='contact-icon-row'>
+                    <i class='fa fa-envelope contact-icon'></i>
+                    <a href='mailto:marcusmcb@gmail.com'>
+                      <h4 className='contact-icon-title'>
+                        marcusmcb@gmail.com
+                      </h4>
+                    </a>
+                  </span>
+                  <span className='contact-icon-row'>
+                    <i class='fa fa-envelope contact-icon'></i>
+                    <a href='mailto:fulltiltremix@gmail.com'>
+                      <h4 className='contact-icon-title'>
+                        fulltiltremix@gmail.com
+                      </h4>
+                    </a>
+                  </span>
+                  <span className='contact-icon-row'>
+                    <i class='fa fa-linkedin contact-icon'></i>
+                    <a href='https://www.linkedin.com/in/marcusmcbride/'>
+                      <h4 className='contact-icon-title'>LinkedIn</h4>
+                    </a>
+                  </span>
+                  <span className='contact-icon-row'>
+                    <i class='fa fa-link contact-icon'></i>
+                    <a href='https://linktr.ee/djmarcusmcb'>
+                      <h4 className='contact-icon-title'>LinkTree (Socials)</h4>
+                    </a>
+                  </span>
+                </Card.Body>
+              </Card>
+            </div>
           </Col>
         </Row>
       </div>
