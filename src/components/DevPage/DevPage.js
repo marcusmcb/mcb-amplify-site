fc0dda1a4a5b4dfc4354335604dc25b48bb38626
Marcus McBride <marcusmcb@gmail.com>
2021-06-01 20:02:37 -0700

    readded card content to dev page

--- src/components/DevPage/DevPage.js
+++ src/components/DevPage/DevPage.js
@@ -12,15 +12,57 @@ const DevPage = () => {
       <div className='dev-page-body'>
         <Row className='g-0'>
           <Col sm={6}>
-            <p>ONE</p>
+            <Card className='dev-page-transparency'>
+              <Card.Body className='dev-page-card'>
+                <span className='dev-page-card-title'>
+                  <p>CoinSnapShot</p>
+                </span>
+                <p className='dev-page-text'>
+                  A responsive, mobile-first crypto dashboard app built with
+                  React and Amplify.
+                </p>
+              </Card.Body>
+            </Card>
           </Col>
-          <Col sm={6}>TWO</Col>
+          <Col sm={6}>
+            <Card className='dev-page-transparency'>
+              <Card.Body className='dev-page-card'>
+                <span className='dev-page-card-title'>
+                  <p>JSP-1 (Javascript Sample Player)</p>
+                </span>
+                <p className='dev-page-text'>
+                  A browser-based drum machine app featuring MIDI/velocity
+                  implementation built with Javascript.
+                </p>
+              </Card.Body>
+            </Card>
+          </Col>
         </Row>
         <Row className='g-0'>
           <Col sm={6}>
-            <p>THREE</p>
+            <Card className='dev-page-transparency'>
+              <Card.Body className='dev-page-card'>
+                <span className='dev-page-card-title'>
+                  <p>Eventageous</p>
+                </span>
+                <p className='dev-page-text'>
+                  A social networking app for live event professionals built with the MERN stack and Redux.
+                </p>
+              </Card.Body>
+            </Card>
           </Col>
-          <Col sm={6}>FOUR</Col>
+          <Col sm={6}>
+            <Card className='dev-page-transparency'>
+              <Card.Body className='dev-page-card'>
+                <span className='dev-page-card-title'>
+                  <p>Yelp Machine Learning Project</p>
+                </span>
+                <p className='dev-page-text'>
+                  A Python/Flask-based machine learning project where our team @ UCI utilized the Yelp API to predict the success of a restaurant based on its category, location and price point using the Radnom Forest methodology.
+                </p>
+              </Card.Body>
+            </Card>
+          </Col>
         </Row>
       </div>
       <Footer />
